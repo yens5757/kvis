4bf82bb368b67c2e0bfd5ca1046476877233ce33
Aiken <chikitlee2001@gmail.com>
2025-01-24 03:14:21 +1100

    update comment on main

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ import os
 
 from config import server_config
 from rdb import read_file
-from globals import global_hashmap, expiry_hashmap
 from server import main as server_main
 
 if __name__ == "__main__":
@@ -13,20 +12,21 @@ if __name__ == "__main__":
     parser.add_argument("--dbfilename", required=False, help="Name of the database file")
     parser.add_argument("--port", required=False, help="Server port")
     parser.add_argument("--replicaof", required=False, help="Replica of master (host port)")
+
     args = parser.parse_args()
 
-    # Populate server_config
+    # Populate the global config
     server_config["dir"] = args.dir
     server_config["dbfilename"] = args.dbfilename
     server_config["port"] = args.port
     server_config["replicaof"] = args.replicaof
 
-    # If we have a valid dir/dbfilename, check if that file exists and read it
+    # Try reading the file
     if server_config["dir"] and server_config["dbfilename"]:
-        file_path = os.path.join(server_config["dir"], server_config["dbfilename"])
-        if os.path.isfile(file_path):
-            print("Reading file:", file_path)
+        path = os.path.join(server_config["dir"], server_config["dbfilename"])
+        if os.path.isfile(path):
+            print(f"[main] Reading RDB file: {path}")
             read_file(server_config["dir"], server_config["dbfilename"])
 
-    # Run the main async server
+    # Run the server
     asyncio.run(server_main())
